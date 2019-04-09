8e525337353b15a1c0c390f1dd995612fe5a6f79
Igor Mostovenko <igormostovenko@gmail.com>
2019-04-09 14:44:06 +0300

    disable axios status validation

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_HOST,
+  validateStatus: status => true,
 })
 
 // here you can define constant's for project or inintial configuration for packages
